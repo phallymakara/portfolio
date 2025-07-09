f8e55d2a09213aecf03dec0effb7687d4fbc7241
phallymakara <macbook@AsuzRog-Strix-G715.local>
2025-07-09 14:49:25 +0700

    add riverpod to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/home_page.dart';
 import 'package:portfolio/l10n/app_localizations.dart';
 import 'package:portfolio/style/app_theme.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.dark,
-      supportedLocales: [Locale('en'), Locale('kh')],
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
+    ProviderScope(
+      child: MaterialApp(
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        darkTheme: AppTheme.dark,
+        themeMode: ThemeMode.dark,
+        supportedLocales: [Locale('en'), Locale('kh')],
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
     ),
   );
 }
--- lib/widget/language_switch.dart
+++ lib/widget/language_switch.dart
@@ -1,44 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:portfolio/constant/app_icon.dart';
 import 'package:portfolio/extension.dart';
+import 'package:portfolio/provider.dart';
 import 'package:portfolio/widget/seo_text.dart';
 
-class LanguageSwitch extends StatelessWidget {
+class LanguageSwitch extends ConsumerWidget {
   const LanguageSwitch({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final locale = ref.watch(appLocalProvider);
     return PopupMenuButton(
       itemBuilder: (context) {
         return [
           PopupMenuItem(
-            value: 1,
+            value: 0,
             child: PopUpLanguageSwitchItem(
               language: 'English',
               icon: AppIcon.us,
             ),
           ),
           PopupMenuItem(
-            value: 0,
+            value: 1,
             child: PopUpLanguageSwitchItem(language: 'ខ្មែរ', icon: AppIcon.km),
           ),
         ];
       },
+      initialValue: locale.value == 'en' ? 0 : 1,
       onSelected: (value) {
         if (value == 0) {
-        } else {}
+          ref.read(appLocalProvider.notifier).changeLocale('en');
+        } else {
+          ref.read(appLocalProvider.notifier).changeLocale('km');
+        }
       },
       child: Row(
         children: [
           Icon(Icons.language, color: context.colorScheme.onBackground),
           const Gap(4),
-          SEOtext(
-            Localizations.localeOf(context).languageCode == 'en'
-                ? 'En'
-                : 'ខ្មែរ',
-          ),
+          SEOtext(locale.value == 'en' ? 'En' : 'ខ្មែរ'),
         ],
       ),
     );
